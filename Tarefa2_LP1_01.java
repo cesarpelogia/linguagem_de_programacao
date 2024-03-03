6866728ca3e3ff731ea8fc070df294b6e3d58547
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-03-03 16:12:53 -0300

    Delete Tarefa2_LP1_01.java

--- Tarefa2_LP1_01.java
+++
@@ -1,7 +0,0 @@
-// Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias.
-// Considerar ano com 365 dias e mês com 30 dias. 
-
-public class Tarefa2_LP1_01 {
-    
-    
-}
