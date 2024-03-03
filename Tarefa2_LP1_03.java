dc5116f6ea31037ca44d17d65dc360709db44c41
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-03-03 16:28:37 -0300

    Delete Tarefa2_LP1_03.java

--- Tarefa2_LP1_03.java
+++
@@ -1,5 +0,0 @@
-// Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste. Calcular e escrever o valor do novo salário. 
-
-public class Tarefa2_LP1_03 {
-    
-}
