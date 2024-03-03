d85f5a6d0cb0fae229586463c115b8b6564541fe
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-03-03 16:30:31 -0300

    Delete Tarefa2_LP1_06.java

--- Tarefa2_LP1_06.java
+++
@@ -1,7 +0,0 @@
-// Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius. 
-// Observação: Para testar se a sua resposta está correta saiba que 100°C = 212°F
-
-
-public class Tarefa2_LP1_06 {
-    
-}
