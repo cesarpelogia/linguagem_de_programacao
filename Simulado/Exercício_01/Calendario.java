30e02f875a3bfea6cfb5123ca6c352507f4c9856
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-05-14 22:00:25 -0300

    Atualizar o Calendario.java

--- Simulado/Exercício_01/Calendario.java
+++ Simulado/Exercício_01/Calendario.java
@@ -15,19 +15,19 @@ public class Calendario {
                 System.out.println("Domingo");
                 break;
             case 2:
-                System.out.println("Segunda");
+                System.out.println("Segunda-feira");
                 break;
             case 3:
-                System.out.println("Terça");
+                System.out.println("Terça-feira");
                 break;
             case 4:
-                System.out.println("Quarta");
+                System.out.println("Quarta-feira");
                 break;
             case 5:
-                System.out.println("Quinta");
+                System.out.println("Quinta-feira");
                 break;
             case 6:
-                System.out.println("Sexta");
+                System.out.println("Sexta-feira");
                 break;
             case 7:
                 System.out.println("Sábado");
